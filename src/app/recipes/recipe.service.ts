b3ea14cfd285d7235efb3ddbb0b2315cfa00775b
Henrik <skinnybeans@gmail.com>
2019-05-06 09:19:07 +1000

    updated recipe detail to show ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe-model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
 
@@ -9,10 +10,12 @@ export class RecipeService {
     private recipes: Recipe[] = [
         new Recipe('Test Recipe',
         'My test recipe',
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'),
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+        [new Ingredient('chicken', 1), new Ingredient('lettuce', 2)]),
         new Recipe('Test Recipe 2',
         'My test recipe 2',
-        'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png')
+        'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+        [new Ingredient('mince', 3), new Ingredient('pasta', 2)])
       ];
 
     getRecipes() {
